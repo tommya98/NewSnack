2579aa001cc1e5eff35e00219e6d18e7691e10c0
Kim <kkk287150155@naver.com>
2023-11-19 17:10:54 +0900

    Refactor : NewsItem component styling

--- src/FeedPage/HomePage/NewsItem/index.tsx
+++ src/FeedPage/HomePage/NewsItem/index.tsx
@@ -3,6 +3,8 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from "@mui/material/IconButton";
+import { useState } from "react";
 
 // Emotion을 사용하여 추가적인 스타일링을 위한 스타일 컴포넌트를 정의합니다.
 const CustomCard = styled(Card)`
@@ -10,6 +12,8 @@ const CustomCard = styled(Card)`
   transition: transform 200ms;
   div {
     margin: 0;
+    padding-left: 0;
+    padding-right: 0;
   }
   box-shadow: none;
   background-color: transparent;
@@ -18,7 +22,43 @@ const CustomCard = styled(Card)`
 const CustomImage = styled("img")`
   object-cover;
   transition: transform 200ms;
-  border-radius: 4px;
+  border-radius: 8px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const MoreButton = styled(IconButton)`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+
+`;
+
+const Title = styled(Typography)`
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28px; /* 140% */
+`;
+
+const Author = styled(Typography)`
+  color: rgba(0, 0, 0, 0.40);
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 12px; /* 80% */
+`;
+
+const Summary = styled(Typography)`
+  color: rgba(0, 0, 0, 0.5);
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 17px; /* 113.333% */
 `;
 
 interface ComponentProps {
@@ -36,32 +76,45 @@ export default function Component({
   thumbnailSrc,
   isLiked,
 }: ComponentProps) {
-  return (
-    <CustomCard>
-      <CardContent sx={{ display: "flex", flexDirection: "row", gap: 2, p: 2 }}>
-        <div style={{ flexShrink: 0 }}>
-          <CustomImage
-            alt="Thumbnail"
-            height="80"
-            src={thumbnailSrc}
-            width="80"
-            style={{
-              aspectRatio: "1 / 1",
-            }}
-          />
-        </div>
-        <div style={{ width: "100%" }}>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            By {author}
-          </Typography>
-          <Typography variant="body2">{summary}</Typography>
-        </div>
-        <MoreVertIcon />
+  const [isHovered, setIsHovered] = useState(false);
 
-      </CardContent>
+  return (
+    <CustomCard 
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    >
+      <Wrapper>
+        <CardContent sx={{ display: "flex", flexDirection: "row", gap: 2, p: 2 }}>
+          <div style={{ flexShrink: 0 }}>
+            <CustomImage
+              alt="Thumbnail"
+              height="90"
+              src={thumbnailSrc}
+              width="135"
+              style={{
+                aspectRatio: "1 / 1",
+              }}
+            />
+          </div>
+          <div style={{ width: "100%", display: "flex", flexDirection: "column", gap: "8px"  }}>
+            <Title>
+              {title}
+            </Title>
+            <Author>
+              By {author}
+            </Author>
+            <Summary>
+              {summary}
+            </Summary>
+          </div>
+          {/* if Hover on, show MoreButton */}
+          {isHovered && 
+          <MoreButton aria-label="more">
+            <MoreVertIcon />
+          </MoreButton>}
+          
+        </CardContent>
+      </Wrapper>
     </CustomCard>
   );
 }
--- src/FeedPage/HomePage/index.tsx
+++ src/FeedPage/HomePage/index.tsx
@@ -3,13 +3,16 @@ import Box from "@mui/material/Box";
 import NewsItem from "./NewsItem";
 import Title from "./Title";
 import SubTitle from "./SubTitle";
+import Divider from '@mui/material/Divider';
 
 const HomePage = () => {
   return (
     <Container>
       <Title date="Today" />
-      <SubTitle label="News" />
+      <Divider />
       <NewsItemContainer>
+      <SubTitle label="News" />
+
       {mockData.map((news) => {
         return (
           <NewsItem
@@ -22,7 +25,10 @@ const HomePage = () => {
         );
       })}
       </NewsItemContainer>
+
+      <NewsItemContainer>
       <SubTitle label="Youtube" />
+
       {mockData.map((news) => {
         return (
           <NewsItem
@@ -34,57 +40,47 @@ const HomePage = () => {
           />
         );
       })}
-      <SubTitle label="Blog" />
-      {mockData.map((news) => {
-        return (
-          <NewsItem
-            key={news.newsTitle}
-            thumbnailSrc={news.imgSrc}
-            title={news.newsTitle}
-            summary={news.newsSummary}
-            author={news.author}
-          />
-        );
-      })}
+      </NewsItemContainer>
     </Container>
   );
 };
 
 const Container = styled(Box)`
   display: flex;
+  max-width: 60%;
   flex-direction: column;
   align-content: center;
   justify-content: center;
-  margin: 2rem 10rem 5rem 10rem;
+  margin: 2rem auto 5rem auto;
   gap: 30px;
 `;
 
 const NewsItemContainer = styled(Box)`
-  gap: 10px;
+  gap: 15px;
 `;
 
 export default HomePage;
 
 const mockData = [
   {
-    imgSrc: "https://picsum.photos/200/300",
-    newsTitle: "뉴스 제목 1",
+    imgSrc: "https://picsum.photos/seed/news1/200/300",
+    newsTitle: "NewsTitle 1",
     newsSummary:
-      "뉴스 기사 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
+      "NewsSummary 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
     author: "author 1",
   },
   {
-    imgSrc: "https://picsum.photos/200/300",
-    newsTitle: "뉴스 제목 2",
+    imgSrc: "https://picsum.photos/seed/news2/200/300",
+    newsTitle: "NewsTitle 2",
     newsSummary:
-      "뉴스 기사 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
+      "NewsSummary 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
     author: "author 2",
   },
   {
-    imgSrc: "https://picsum.photos/200/300",
-    newsTitle: "뉴스 제목 3",
+    imgSrc: "https://picsum.photos/seed/news3/200/300",
+    newsTitle: "NewsTitle 3",
     newsSummary:
-      "뉴스 기사 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
+      "NewsSummary 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe vero repudiandae deleniti vitae nam nihil officia omnis porro modi nulla laboriosam, dolorem, vel commodi nostrum, ea asperiores consequatur optio at.",
     author: "author 3",
   },
 ];
